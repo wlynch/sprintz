29986c599eaf5117c5992fa539d2da1c4415efdf
Billy Lynch <wlynch92@gmail.com>
2014-04-05 17:48:17 -0400

    Added basic typing

---
+++ js/sprintz.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function() {
+	$(document).keypress(keyHandler);
+});
+
+
+function keyHandler(e) {
+	var key = String.fromCharCode(e.which);
+	var word = $('#incomplete').text().trim();
+	if (key == word.charAt(0)) {
+		$('#incomplete').text(word.slice(1));
+		$('#completed').append(key);
+	} else {
+		$("#incorrect").text(parseInt($("#incorrect").text()) + 1);
+	}
+	
+	var word = $('#incomplete').text().trim();
+	console.log(word.length);
+	if ( word.length == 0 ) {
+		newWord();	
+	}
+}
+
+var words = ["asdf", "qwer", "zxcv"]
+
+function newWord() {
+	var nw = words[Math.floor(Math.random()*words.length)];
+	console.log(nw);
+	$("#incomplete").text(nw);
+	$("#completed").text("");
+}
